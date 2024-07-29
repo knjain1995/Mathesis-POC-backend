6b5a473ce92d06dd5dbbdfa92afe145f5e4628d6
knjain1995 <knjain1995@gmail.com>
2024-07-29 22:31:16 +0530

    Added functionality to authenticate Login and check for duplicate email and phone number while adding or updating Sign Up Information

--- src/main/java/com/example/demo/SignUpInformationController.java
+++ src/main/java/com/example/demo/SignUpInformationController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.Scanner;
 
 // @RestController: indicates that class is a controller which handles web requests it @Controller + @ResponseBody - handles http requests and response in response body
 // @RequestMapping: maps http requests to specified path to this controller class. All requests to this API will be handled by methods in this class
@@ -26,6 +27,7 @@ public class SignUpInformationController {
     @GetMapping
     public ResponseEntity<List<SignUpInformation>> getAllSignUpInformation() {
         List<SignUpInformation> allSignUpInformation = signUpInformationService.getAllSignUpInformation();
+//        System.out.println(allSignUpInformation);
         return new ResponseEntity<>(allSignUpInformation, HttpStatus.OK);
     }
 
@@ -33,25 +35,37 @@ public class SignUpInformationController {
     @GetMapping("/{signUpID}")
     public ResponseEntity<SignUpInformation> getSignUpInformationById(@PathVariable String signUpID) {
         Optional<SignUpInformation> signUpInformation = signUpInformationService.getSignUpInformationById(signUpID);
+//        System.out.println(signUpInformation);
         return signUpInformation.map(res -> new ResponseEntity<>(res, HttpStatus.OK)).orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
     }
 
     // maps HTTP POST requests to api/signup to the method signUpInformation
     @PostMapping
     public ResponseEntity<SignUpInformation> signUpInformation(@RequestBody SignUpInformation signUpInformation) {  //get signup information as response body
-        SignUpInformation createdSignUpInformation = signUpInformationService.createSignUpInformation(signUpInformation);
-        return new ResponseEntity<>(createdSignUpInformation, HttpStatus.CREATED);
+        try {
+            if(signUpInformationService.isEmailDuplicate(signUpInformation.getEmail()) | signUpInformationService.isPhoneDuplicate(signUpInformation.getPhone())) {
+                return new ResponseEntity<>(null, HttpStatus.CONFLICT);
+            }
+            SignUpInformation createdSignUpInformation = signUpInformationService.createSignUpInformation(signUpInformation);
+            return new ResponseEntity<>(createdSignUpInformation, HttpStatus.CREATED);
+        }
+        catch (RuntimeException e) {
+            return new ResponseEntity<>(null, HttpStatus.BAD_REQUEST);
+        }
     }
 
     // maps HTTP PUT request for specified id to api/ signup to the method signUpInformation
     @PutMapping("/{signUpID}")
     public ResponseEntity<SignUpInformation> updateSignUpInformation(@PathVariable String signUpID, @RequestBody SignUpInformation signUpInformation) {
         try {
+            if(signUpInformationService.isEmailDuplicate(signUpInformation.getEmail(), signUpID) | signUpInformationService.isPhoneDuplicate(signUpInformation.getPhone(), signUpID)) {
+                return new ResponseEntity<>(null, HttpStatus.CONFLICT);
+            }
             SignUpInformation updatedSignUpInformation = signUpInformationService.updateSignUpInformation(signUpID, signUpInformation);
             return new ResponseEntity<>(updatedSignUpInformation, HttpStatus.OK);
         }
         catch (RuntimeException e) {
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+            return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
         }
     }
 
@@ -60,11 +74,18 @@ public class SignUpInformationController {
     public ResponseEntity<SignUpInformation> deleteSignUpInformation(@PathVariable String signUpID) {
         try {
             Optional<SignUpInformation> deletedSignUpInformation = signUpInformationService.deleteSignUpInformation(signUpID);
-            return deletedSignUpInformation.map(res -> new ResponseEntity<>(res, HttpStatus.OK)).orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
+            return deletedSignUpInformation.map(res -> new ResponseEntity<>(res, HttpStatus.OK)).orElse(new ResponseEntity<>(null, HttpStatus.NOT_FOUND));
         }
         catch (RuntimeException e) {
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
+    }
+
+    // maps HTTP Post request with login credentials to api/signup/login to check if login data is present and return SignUp Information
+    @PostMapping("/checkLoginCredentials")
+    public ResponseEntity<SignUpInformation> checkLoginCredentials(@RequestBody LoginInformation loginInformation) {
+        Optional<SignUpInformation> loggedInSignUpData = signUpInformationService.findSignUpInformationByEmailAndPassword(loginInformation);
+        return loggedInSignUpData.map(res-> new ResponseEntity<>(res, HttpStatus.OK)).orElse(new ResponseEntity<>(null, HttpStatus.NOT_FOUND));
     }
 
 }
--- src/main/java/com/example/demo/SignUpInformationRepository.java
+++ src/main/java/com/example/demo/SignUpInformationRepository.java
@@ -2,8 +2,13 @@ package com.example.demo;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.Optional;
+
 // REPOSITORY INTERFACE
 // Our document and ID's type are the parameters in the form of a Generic
 public interface SignUpInformationRepository extends MongoRepository<SignUpInformation, String> {
 //    by extending MongoRepository, we now have methods for performing common database operations save, delete, find etc
+    Optional<SignUpInformation> findByEmailAndPassword (String email, String password); // Get document based on Email and Password. Used in login authentication
+    Optional<SignUpInformation> findByEmail(String email);  // Used to find document using the Email as parameter. Used to check duplication during Creating and Updating Sign Up Information
+    Optional<SignUpInformation> findByPhone(String phone);  // Used to find document using the Phone Number as parameter. Used to check duplication during Creating and Updating Sign Up Information
 }
